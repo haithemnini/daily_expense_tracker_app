b05ca04283c5e3b09be85cb74412fed0e3e17e1e
Haithem Nini <HaithemNini@outlook.com>
2024-01-25 21:32:33 +0100

    - added  flutter_speed_dial libray - updated home view desgin without logic

--- lib/features/home/home_view.dart
+++ lib/features/home/home_view.dart
@@ -1,12 +1,8 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../../core/extensions/extensions.dart';
 import 'main/main_view.dart';
 import 'stat/stat_view.dart';
-import 'widget/app_bottom_nav_bar.dart';
+import 'widgets/widgets.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -28,35 +24,11 @@ class _HomeViewState extends State<HomeView> {
         currentIndex: _currentIndex,
         onTabTapped: (int index) => setState(() => _currentIndex = index),
       ),
-      floatingActionButton: _buildFAB(context),
+      floatingActionButton: const AppFloatingActionButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25),
         child: _pages.elementAt(_currentIndex),
-      ),
-    );
-  }
-
-  _buildFAB(BuildContext context) {
-    return FloatingActionButton(
-      shape: const CircleBorder(),
-      // elevation: 0,
-      onPressed: () {},
-      child: Container(
-        height: 60,
-        width: 60,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          gradient: LinearGradient(
-            colors: [
-              context.colorScheme.tertiary,
-              context.colorScheme.secondary,
-              context.colorScheme.primary,
-            ],
-            transform: const GradientRotation(pi / 4),
-          ),
-        ),
-        child: const Icon(FontAwesomeIcons.plus),
       ),
     );
   }
--- lib/features/home/stat/stat_view.dart
+++ lib/features/home/stat/stat_view.dart
@@ -1,4 +1,4 @@
-import 'package:daily_expense_tracker_app/core/extensions/extensions.dart';
+import '../../../core/extensions/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
